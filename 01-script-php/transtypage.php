f93883bf3ec69fbc09e3898252fe5b1bcc4045f6
tentacode <contact@gabrielpillet.com>
2018-03-14 12:15:06 +0100

    Transtypage des variables

---
+++ 01-script-php/transtypage.php
@@ -0,0 +1,15 @@
+<?php
+
+$captainAge = 33;
+$captainAgeString = (string)$captainAge; // traduit en string
+
+var_dump($captainAge);
+var_dump($captainAgeString); // "23"
+var_dump((bool)1337); // true
+var_dump((bool)0); // false
+var_dump((bool)-1); // true
+var_dump((float)-1); // -1
+var_dump((int)3.9); // 3
+var_dump((bool)"true"); // true
+var_dump((bool)"false"); // true
+var_dump((bool)""); // false
