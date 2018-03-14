e90460967ff349f352225c7800851d972359af9f
tentacode <contact@gabrielpillet.com>
2018-03-14 11:14:48 +0100

    Fichier qui explique comment faire des commentaires
    Mais si vous mettez pleins de commentaires ça veut dire que votre code est moche

---
+++ 01-script-php/commentaires.php
@@ -0,0 +1,15 @@
+<?php
+
+// commentaire sur une seule ligne
+print "\tCoucou\n"; // retour à la ligne interprêté
+print '\tCoucou\n'; // non interprêté
+
+/*
+    Commentaire
+    sur plusieurs
+    lignes
+*/
+print 'C\'est finit'; // echaper le guillemet simple
+die("\nC'est vraiment finit\n");
+
+# ne pas utiliser ce genre de commentaire
